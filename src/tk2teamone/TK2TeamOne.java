68e2d84cfc73b424ec701c824a15ef019ac9a1d5
dhea <widheaayuramadhani@gmail.com>
2026-09-21 17:56:06 +0700

    Menambahkan implementasi class Buah Mangga

--- src/tk2teamone/TK2TeamOne.java
+++ src/tk2teamone/TK2TeamOne.java
@@ -15,6 +15,19 @@ public class TK2TeamOne {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        //Bagian Dhea
+        BuahMangga Mangga =  new BuahMangga("Mangga Madu","Kuning","Manis","Rp30.000/kg");
+        Mangga.setJenis("Gadung");
+        Mangga.setWarna("Hijau");
+        Mangga.setRasa("Manis");
+        Mangga.setHarga("Rp20.000/kg");
+        System.out.println("=====MANGGA - DHEA=====");
+        System.out.println("Jenis : " + Mangga.getJenis());
+        System.out.println("Warna : " + Mangga.getWarna());
+        System.out.println("Rasa : " + Mangga.getRasa());
+        System.out.println("Harga : " + Mangga.getHarga());
+        System.out.println("=======================\n");
+                
     }
     
 }
