5185f16ad88ecee7882274e0a6947bd137873008
Hamdan <hamdanabialmujahidin@gmail.com>
2026-09-21 17:04:21 +0700

    menambahkan class buah semangka

---
+++ src/tk2teamone/BuahSemangka.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tk2teamone;
+
+/**
+ *
+ * @author kazuk
+ */
+public class BuahSemangka {
+    String jenis;
+    String warna;
+    String rasa;
+    String harga;
+
+    public BuahSemangka(String jenis, String warna, String rasa, String harga) {
+        this.jenis = jenis;
+        this.warna = warna;
+        this.rasa = rasa;
+        this.harga = harga;
+    }
+
+    public String getJenis() {
+        return jenis;
+    }
+
+    public void setJenis(String jenis) {
+        this.jenis = jenis;
+    }
+
+    public String getWarna() {
+        return warna;
+    }
+
+    public void setWarna(String warna) {
+        this.warna = warna;
+    }
+
+    public String getRasa() {
+        return rasa;
+    }
+
+    public void setRasa(String rasa) {
+        this.rasa = rasa;
+    }
+
+    public String getHarga() {
+        return harga;
+    }
+
+    public void setHarga(String harga) {
+        this.harga = harga;
+    }
+    
+}
